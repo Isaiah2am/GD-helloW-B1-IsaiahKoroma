6b389b5eaaa8ac488abba176c04fae13bc04eebf
Isaiah2am <isaiahkoroma@hotmail.com>
2025-01-21 09:46:06 +0100

    m2bo version

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movementSpeed = 4;
+
+    /*
+        Hier komt het volgende blok code
+    */
+
+    public Animator _animator;
+
+
+
+
+    void Update()
+    { 
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            _animator.SetFloat("speed", 1);
+
+        } else
+        {
+            _animator.SetFloat("speed", 0);
+        }
+
+    }
+}
+
